65de60be96000524ba438a89a77eff459c9d564c
aalohaa <khusainov.alizhan@bk.ru>
2024-08-07 19:42:21 +0500

    server created & start page started

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const port = 4000
+
+const index = require('./routes/index')
+
+app.use('/views', express.static(path.join(__dirname + '/views')))
+app.use('/public', express.static(path.join(__dirname + '/public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', index)
+
+app.listen(port, () => {
+    console.log(`app is running on port http://localhost:${port}`)
+})
