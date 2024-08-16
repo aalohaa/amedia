ec09b9e906c31b4751995432c1dafe34e27394fd
aalohaa <khusainov.alizhan@bk.ru>
2024-08-16 16:48:09 +0500

    auth and auth ui are completed

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.post('/sign-up', async (req, res) => {
     let first_name = req.body.first_name;
     let last_name = req.body.last_name;
     let email = req.body.email.toLowerCase().trim();
-    let password = req.body.password
+    let password = req.body.password.trim();
 
     if(!email && !password) {
         res.send(r);
@@ -23,6 +23,7 @@ router.post('/sign-up', async (req, res) => {
             email: email
         }).then(() => {
             r['r'] = 1;
+            r['first_name'] = first_name;
             res.send(r);
         }).catch((e) => {
             console.log(e);
@@ -38,8 +39,9 @@ router.post('/sign-in', async (req, res) => {
     let password = req.body.password;
 
     await fauth.signInWithEmailAndPassword(fauth.getAuth(), email, password).then(async(userCredential) =>{
-        await fdb.collection('users').doc(userCredential.user.uid).get().then(()=> {
+        await fdb.collection('users').doc(userCredential.user.uid).get().then((userDoc)=> {
             r['r'] = 1;
+            r['first_name'] = userDoc.data().first_name;
             res.send(r);
         })
     }).catch((e)=>{
