561d8c690ae63bedd920ff2ee3bb5cbfd17505cf
aalohaa <khusainov.alizhan@bk.ru>
2024-09-27 15:36:23 +0500

    google auth is done. font added & ui html for media player is done..

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,48 @@ const express = require('express');
 const router = express.Router();
 const fauth = require('../libs/firebase').fauth;
 const fdb = require('../libs/firebase').fdb;
+const passport = require('passport');
+const googleConf = require('../google/google.json').web
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+})
+
+passport.use(
+    "google_sign_in",
+    new GoogleStrategy(
+        {
+            clientID: googleConf.client_id,
+            clientSecret: googleConf.client_secret,
+            callbackURL: googleConf.redirect_uris[0],
+            passReqToCallback: true
+        },
+        function (request, accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+        }
+    )
+);
+
+passport.use(
+    "google_sign_up",
+    new GoogleStrategy(
+        {
+            clientID: googleConf.client_id,
+            clientSecret: googleConf.client_secret,
+            callbackURL: googleConf.redirect_uris[1],
+            scope: ["email", "profile"],
+            passReqToCallback: true
+        },
+        function (request, accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+        }
+    )
+);
 
 router.post('/sign-up', async (req, res) => {
     var r = { r: 0 };
@@ -66,6 +108,83 @@ router.post('/sign-in', async (req, res) => {
         res.send(r);
     }
 })
+
+// google auth
+
+router.get(
+    "/google_sign_in",
+    passport.authenticate("google_sign_in", { scope: ["profile", "email"] }, () => { })
+)
+
+router.get(
+    "/google_sign_in/index",
+    passport.authenticate("google_sign_in", { failureRedirect: "/?status=0" }),
+    async function (req, res) {
+        const google_id = req.user.id;
+        let documentFound = false;
+
+        try {
+            await fdb.collection("users").where('google_id', '==', google_id).get().then((users) => {
+                if (!users.empty) {
+                    users.forEach(user => {
+                        if (user.exists) {
+                            documentFound = true;
+                            req.session.isAuth = true;
+                            req.session.first_name = user.data().first_name;
+                            res.redirect('/');
+                        }
+                    })
+                }
+            })
+
+            if (!documentFound) {
+                res.redirect('/?status=0'); 
+            }
+        } catch (e) {
+            console.log(e);
+            return res.redirect('/?status=0');
+        }
+    }
+);
+
+router.get(
+    "/google_sign_up",
+    passport.authenticate("google_sign_up", { scope: ["profile", "email"] }, () => { })
+)
+
+router.get(
+    "/google_sign_up/index",
+    passport.authenticate("google_sign_up", { failureRedirect: "/?status=1" }),
+    async function (req, res) {
+        const google_id = req.user.id;
+        const email = req.user.emails[0].value;
+        const first_name = req.user.name.givenName;
+
+        try {
+            await fauth.createUserWithEmailAndPassword(fauth.getAuth(), email, "google5a9c8f4e2").then(async (userCredential) => {
+                const user_id = userCredential.user.uid;
+
+                await fdb.collection('users').doc(user_id).set({
+                    user_id: user_id,
+                    google_id: google_id,
+                    email: email,
+                    first_name: first_name,
+                    last_name: null
+                })
+
+                req.session.isAuth = true;
+                req.session.first_name = first_name;
+                return res.redirect('/');
+            })
+        } catch (e) {
+            console.log(e);
+            if (e.code == 'auth/email-already-in-use') {
+                return res.redirect('/?status=2')
+            }
+        }
+    }
+)
+// google auth end
 
 router.post('/reset-password', async (req, res) => {
     var r = { r: 0 };
